687cb99ed16d582e42663bcc4f9efd336a9fbf0a
Michael Vaganov <mvaganov@hotmail.com>
2018-05-07 14:22:49 -0700

    Revert "Revert "hand controller fixes""
    This reverts commit d7f11167a4d0c322b86e0d7674930bbf947cdf70, reversing
    changes made to 84e7446379717820d9d820793d0e5e3f87ead772.

--- Assets/NonStandard Assets/Controls/NSVRTK_InputSimulator.cs
+++ Assets/NonStandard Assets/Controls/NSVRTK_InputSimulator.cs
@@ -197,8 +197,17 @@
             destroyed = true;
         }
 
-        // private void Update()
-        // {
+        private int lastKnownMoveControllerIndex = -1;
+        private void FixedUpdate() {
+            if(hc.GetMoveControllerIndex() != lastKnownMoveControllerIndex) {
+                lastKnownMoveControllerIndex = hc.GetMoveControllerIndex();
+                for(int i = 0; i < hc.moveControllerRoots.Length; ++i) {
+                    hc.moveControllerRoots[i].GetComponent<SDK_ControllerSim>().Selected = false;
+                }
+                hc.moveControllerRoots[lastKnownMoveControllerIndex].GetComponent<SDK_ControllerSim>().Selected = true;
+            }
+        }
+        // private void Update() {
         //     // if (Input.GetKeyDown(hc.toggleInstructionsKey))
         //     // {
         //     //     showControlHints = !showControlHints;
@@ -229,21 +238,21 @@
         //     //    }
         //     //}
 
-        //     //if (Input.GetKeyDown(changeHands))
-        //     //{
-        //     //    if (currentHand.name == "LeftHand")
-        //     //    {
-        //     //        currentHand = rightHand;
-        //     //        rightController.Selected = true;
-        //     //        leftController.Selected = false;
-        //     //    }
-        //     //    else
-        //     //    {
-        //     //        currentHand = leftHand;
-        //     //        rightController.Selected = false;
-        //     //        leftController.Selected = true;
-        //     //    }
-        //     //}
+            // if (Input.GetKeyDown(changeHands))
+            // {
+            //    if (currentHand.name == "LeftHand")
+            //    {
+            //        currentHand = rightHand;
+            //        rightController.Selected = true;
+            //        leftController.Selected = false;
+            //    }
+            //    else
+            //    {
+            //        currentHand = leftHand;
+            //        rightController.Selected = false;
+            //        leftController.Selected = true;
+            //    }
+            // }
 
         //     //if (isHand)
         //     //{
