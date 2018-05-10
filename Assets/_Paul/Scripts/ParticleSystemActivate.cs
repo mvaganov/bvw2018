009aaf6a8a139ee47856c20c945d5d5cd15d4492
Paul <pkomin20@SHS04692.shs.shschools.org>
2018-05-10 13:15:19 -0700

    My big commit

---
+++ Assets/_Paul/Scripts/ParticleSystemActivate.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleSystemActivate : MonoBehaviour {
+
+	public ParticleSystem RewindParticle;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void ActivateParticleSystem()
+	{
+		if (RewindParticle.isPlaying) {
+			RewindParticle.Stop ();
+		} else {
+			RewindParticle.Play ();
+		}
+	}
+}
