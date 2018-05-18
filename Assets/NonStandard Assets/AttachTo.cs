83b8346fa4f5b08530a5b2e7a8829391012f4f3e
Mr. V <mvaganov@shschools.org>
2018-05-17 18:50:05 -0700

    return belt
    * created script to attach itself to the player's 'belt'
    * cube on the player's belt will bring them back home

---
+++ Assets/NonStandard Assets/AttachTo.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttachTo : MonoBehaviour {
+	[System.Serializable]
+	public struct IdealParent {
+		public string name;
+		public Vector3 offset;
+	}
+	public IdealParent[] idealParent;
+
+	void FixedUpdate () {
+		for (int i = 0; i < idealParent.Length; ++i) {
+			GameObject go = GameObject.Find (idealParent[i].name);
+			if (go != null) {
+				transform.parent = go.transform;
+				transform.localPosition = idealParent[i].offset;
+				Destroy (this);
+			}
+		}
+	}
+}
