dc30ddfdb744487acf86ed4d77beb46ae2daa639
sasha <sbellack20@shschools.org>
2018-05-18 09:05:15 -0700

    wands level

---
+++ Assets/_Sasha/UETools/Magic_Particle_Effects/Scripts/Wand.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wand : MonoBehaviour {
+	VRTK.VRTK_InteractableObject io;
+	public ParticleSystem particles; 
+	// Use this for initialization
+	void Start () {
+
+		io = GetComponent < VRTK.VRTK_InteractableObject> ();
+		if (io == null) {
+			io = gameObject.AddComponent<VRTK.VRTK_InteractableObject> ();
+		}
+		io.isUsable = io.isGrabbable = true;
+		if (particles == null) {
+			particles = GetComponentInChildren<ParticleSystem> ();
+		}
+		particles.Stop ();
+		io.InteractableObjectUsed += (sender,EventArgs) => {particles.Play(); };
+		io.InteractableObjectUnused += (sender,EventArgs) => {particles.Stop(); };
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
